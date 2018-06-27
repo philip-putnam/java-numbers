e9d209281948a3ba8aaf4afbe699882d0912235d
Philip Putnam <putnamp989@gmail.com>
2018-06-26 19:11:15 -0700

    get user integer input and double it

--- src/main/java/javaNumbers.java
+++ src/main/java/javaNumbers.java
@@ -1,6 +1,22 @@
-public class DoubleIt {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class javaNumbers {
     public static void main(String[] args) {
         System.out.println("Give me a number and I'll double it for you!");
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            String stringNumber = bufferedReader.readLine();
+            Integer yourNumber = Integer.parseInt(stringNumber);
+            Integer yourDoubledNumber = yourNumber * 2;
 
+            System.out.println("I doubled your number for you: " + yourDoubledNumber);
+
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
